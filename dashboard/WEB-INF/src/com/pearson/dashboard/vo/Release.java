9422ea7695b3dfd8b0a7586d42a2ec47ff030059
Mohammed Saquib <msaqib@hcl.com>
2015-02-01 20:55:49 +0530

    Revert "Deleted old project"
    This reverts commit 7e4ad8f87df5246e0530e2e7c9cba83b010cf3ae.

---
+++ dashboard/WEB-INF/src/com/pearson/dashboard/vo/Release.java
@@ -0,0 +1,30 @@
+package com.pearson.dashboard.vo;
+
+public class Release {
+	
+	private String releaseName;
+	private String releaseStartDate;
+	private String releaseEndDate;
+	
+	public String getReleaseName() {
+		return releaseName;
+	}
+	public void setReleaseName(String releaseName) {
+		this.releaseName = releaseName;
+	}
+	public String getReleaseStartDate() {
+		return releaseStartDate;
+	}
+	public void setReleaseStartDate(String releaseStartDate) {
+		this.releaseStartDate = releaseStartDate;
+	}
+	public String getReleaseEndDate() {
+		return releaseEndDate;
+	}
+	public void setReleaseEndDate(String releaseEndDate) {
+		this.releaseEndDate = releaseEndDate;
+	}
+	
+	
+
+}
